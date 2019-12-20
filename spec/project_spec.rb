8c3724c26d601da87b135f20211e901a94d59ecb
Eva Wiedmann <evawiedmann@gmail.com>
2019-12-20 14:22:36 -0800

    add three passing tests, one failing

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -9,11 +9,22 @@ end
 
 describe("#save") do
   it('saves word')do
-  word = Word.new('Grass', nil, 'definition')
+  word = Word.new('Blue', nil, 'definition')
   word.save()
   expect(Word.all).to(eq([word]))
 end
-
-
 end
+describe ('.all') do
+  it('is empty')do
+  expect(Word.all()).to(eq([]))
+end
+end
+describe('.clear') do
+    it('clears all words') do
+      word = Word.new('Blue', nil, 'definition')
+      word.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
+  end
 end
